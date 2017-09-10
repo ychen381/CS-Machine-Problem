d328725dfb4028bf7daa4b7142e2b617f212d19d
ychen381 <ychen381@b7fedb43-3d08-4265-b3b9-89e4cbc7bd95>
2017-09-10 22:21:33 +0000

    mp1 subission

--- mp1/mp1.cpp
+++ mp1/mp1.cpp
@@ -1,5 +1,35 @@
 #include <string>
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include "cs225/PNG.h"
+#include "cs225/HSLAPixel.h"
+using namespace cs225;
+using namespace std;
 
 void rotate(std::string inputFile, std::string outputFile) {
+    PNG* original=new PNG();
+    original->readFromFile(inputFile);
+    unsigned width_=original->width();
+    unsigned height_=original->height();
+    PNG* output=new PNG();
+    output->resize(width_,height_);
+
+    for (unsigned x=0;x< width_;x++){
+      for(unsigned y=0;y<height_;y++){
+
+        *(output->getPixel(width_-1-x,height_-1-y))=*(original->getPixel(x,y));
+
+      }
+    }
+    output->writeToFile(outputFile);
+    delete original;
+    delete output;
+
+
+    original=NULL;
+    output=NULL;
+
+
 
 }
